8b9f69824bced27a18553a9a248f28210f3e9883
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-20 10:49:50 +0100

    Corrigindo a api adicionando [FromBody]

--- GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -31,7 +31,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
             if (vo == null) return BadRequest();
             var product = await _repository.Create(vo);
@@ -39,7 +39,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
             if (vo == null) return BadRequest();
             var product = await _repository.Update(vo);
