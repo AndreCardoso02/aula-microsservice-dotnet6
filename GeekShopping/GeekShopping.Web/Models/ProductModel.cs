81ba5c41241af10749df7986cbad5d3f7e906d36
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-20 10:22:29 +0100

    Adicionando a interface de product service

---
+++ GeekShopping/GeekShopping.Web/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class ProductModel
+    {
+        public long Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public required string CategoryName { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
