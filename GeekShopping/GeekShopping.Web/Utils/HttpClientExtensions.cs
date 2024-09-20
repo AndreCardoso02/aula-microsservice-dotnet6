203910e6114dfbbad330592a7821db0571bc548a
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-20 10:34:52 +0100

    Criando o método de extensão para consumir uma API

---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
+
+
+        // Metodo responsavel por ler os dados
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode) 
+                throw new ApplicationException($"Something went wrong calling API:" + 
+                                               $"{response.ReasonPhrase}");
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString
+                                       , new JsonSerializerOptions 
+                                       { PropertyNameCaseInsensitive = true });
+        }
+
+        // Metodo responsavel por fazer o post e put
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+
+            // Montar a request
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PostAsync(url, content);
+        }
+
+
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+
+            // Montar a request
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return httpClient.PutAsync(url, content);
+        }
+    }
+}
