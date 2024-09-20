379ead130f86f109eddee004680b061ce9f3387d
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-20 10:17:42 +0100

    Convertendo a aplicação de web pages para MVC

--- GeekShopping/GeekShopping.Web/Program.cs
+++ GeekShopping/GeekShopping.Web/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +23,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
