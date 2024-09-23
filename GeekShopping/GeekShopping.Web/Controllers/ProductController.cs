85068f3d5c24ab9be9819e376d0cf1926e0b0294
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-23 09:01:41 +0100

    Fechando o crud de producto no parte do cliente

--- GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -18,5 +18,67 @@ namespace GeekShopping.Web.Controllers
             var products = await _service.FindAllProducts();
             return View(products);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductModel model)
+        {
+            if (ModelState.IsValid) {
+                var response = await _service.CreateProduct(model);
+                if (response != null)
+                    return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(long id)
+        {
+            if (id <= 0) return NotFound();
+            var product = await _service.FindProductById(id);
+            if (product == null || product.Id <= 0) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _service.UpdateProduct(model);
+                if (response != null)
+                    return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(long id)
+        {
+            if (id <= 0) return NotFound();
+            var product = await _service.FindProductById(id);
+            if (product == null || product.Id <= 0) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(ProductModel model)
+        {
+            if (model.Id > 0)
+            {
+                var status = await _service.DeleteProductById(model.Id);
+                if (status)
+                    return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index), new { id = model.Id});
+        }
     }
 }
