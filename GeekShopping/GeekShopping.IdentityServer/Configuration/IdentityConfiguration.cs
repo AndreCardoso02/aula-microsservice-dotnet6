75e276db7847b5205b33bbec348dbb5ec63fbbc2
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-26 10:46:09 +0100

    Configuracoes do Identity: IdentityConfiguration e Program

---
+++ GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        // Identity resource, nomes de grupos de claims que podem ser protegido
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        // Identity Scope - Objectos com informações do perfil (Nome, Sobrenome, Username), Identity Claim
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>()
+            {
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data.")
+            };
+
+        // Client - componente de software que requisita um recurso a um server
+        public static IEnumerable<Client> Clients =>
+            new List<Client>()
+            {
+                new Client // client generico
+                {
+                    ClientId = "client",
+                    ClientSecrets =
+                    {
+                        new Secret("my_super_secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes =
+                    {
+                        "read", "write", "profile"
+                    }
+                },
+                new Client // client especifico do geek
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets =
+                    {
+                        new Secret("my_super_secret".Sha256())
+                    },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"http://localhost:44396/signin-oidc"}, // porta ssl: 44396 ; porta normal = 46179
+                    PostLogoutRedirectUris = { "http://localhost:44396/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+                }
+            };
+    }
+}
--- GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,4 +1,40 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Adicionando o DbContext
+// Adicionando a string de conexão
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+{
+    options.UseMySql(connection,
+                    new MySqlServerVersion(
+                        new Version(8, 0, 38)));
+});
+
+// adicionando o identity server
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentity = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builderIdentity.AddDeveloperSigningCredential();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,6 +49,9 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// adicionando o identity duende
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
