b026d6d5b57b84573c5121931225a7efd87b0c04
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-09-26 10:21:02 +0100

    Configurando o identity server

---
+++ GeekShopping/GeekShopping.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+    }
+}
