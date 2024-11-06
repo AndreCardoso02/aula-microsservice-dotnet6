6e265603ab487b4d22c3c815a62c891d47790f84
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-11-06 15:04:08 +0000

    Adicionando o profile service

--- GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,9 @@
+using Duende.IdentityServer.Services;
 using GeekShopping.IdentityServer.Configuration;
 using GeekShopping.IdentityServer.Initializer;
 using GeekShopping.IdentityServer.Model;
 using GeekShopping.IdentityServer.Model.Context;
+using GeekShopping.IdentityServer.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -39,6 +41,7 @@ var builderIdentity = builder.Services.AddIdentityServer(options =>
 
 // Ijecao de dependencia
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
 
 
 builderIdentity.AddDeveloperSigningCredential();
