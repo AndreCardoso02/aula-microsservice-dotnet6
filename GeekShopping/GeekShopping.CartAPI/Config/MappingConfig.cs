effd818033782ff60e7bee568f7a0ff18028dbf9
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-11-29 08:59:43 +0000

    Adding mapping Model->VO

--- GeekShopping/GeekShopping.CartAPI/Config/MappingConfig.cs
+++ GeekShopping/GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
 using GeekShopping.CartAPI.Model;
 
 namespace GeekShopping.CartAPI.Config
@@ -9,7 +10,10 @@ namespace GeekShopping.CartAPI.Config
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-                //config.CreateMap<ProductVO, Product>().ReverseMap();
+                config.CreateMap<ProductVO, Product>().ReverseMap();
+                config.CreateMap<CartHeaderVO, CartHeader>().ReverseMap();
+                config.CreateMap<CartDetailVO, CartDetail>().ReverseMap();
+                config.CreateMap<CartVO, Cart>().ReverseMap();
             });
 
             return mappingConfig;
