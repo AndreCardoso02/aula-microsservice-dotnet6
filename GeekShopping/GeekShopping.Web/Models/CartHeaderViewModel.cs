cb7cf591fade3e50903d470e2336b1051a88f748
AndreCardoso02 <andremirandacardoso02@gmail.com>
2024-12-02 08:55:36 +0000

    Adicionando o cart header model

--- GeekShopping/GeekShopping.Web/Models/CartHeaderViewModel.cs
+++ GeekShopping/GeekShopping.Web/Models/CartHeaderViewModel.cs
@@ -7,5 +7,6 @@ namespace GeekShopping.Web.Models
         public long Id { get; set; }
         public string UserId { get; set; }
         public string CouponCode { get; set; }
+        public double PorchaseAmount { get; set; }
     }
 }
