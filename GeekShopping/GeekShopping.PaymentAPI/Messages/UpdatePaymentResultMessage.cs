03d08efafd692216637e0537e842fddec08582a7
AndreCardoso02 <andremirandacardoso02@gmail.com>
2025-01-13 10:52:47 +0100

    Add Messages

---
+++ GeekShopping/GeekShopping.PaymentAPI/Messages/UpdatePaymentResultMessage.cs
@@ -0,0 +1,10 @@
+using GeekShopping.MessageBus;
+
+namespace GeekShopping.PaymentAPI.Messages;
+
+public class UpdatePaymentResultMessage : BaseMessage
+{
+    public long OrderId { get; set; }
+    public bool Status { get; set; }
+    public string Email { get; set; }
+}
--- GeekShopping/GeekShopping.PaymentAPI/Program.cs
+++ GeekShopping/GeekShopping.PaymentAPI/Program.cs
@@ -1,9 +1,65 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddControllers();
+
+// Configura�]oes de seguranca
+builder.Services.AddAuthentication("Bearer") // Autentica��o
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435"; // Identity Server
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+// Authorizacao
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "GeekShopping.PaymentAPI" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
